ab2c5822e4a7e980103894dd290a888d573ef6fa
SHREYA-GEM <lbt21cs113@gmail.com>
2025-07-23 20:13:43 +0530

    Removed unnecessary files

--- Lexipal_flutterApp/test_req.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://192.168.137.155:5001/gaze/gaze_detection"
-file_path = "D:/Flutter_Kunji/hello_worrld/test.jpg"
-
-with open(file_path, "rb") as img:
-    response = requests.post(url, files={"image": img})
-
-print(response.json())  # Check response from the server
